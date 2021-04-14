d4bb13a9e9740bdc69112499b1fabe14a951f5a9
Upos1997 <37412723+Upos1997@users.noreply.github.com>
2021-04-14 19:00:05 +0200

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,36 @@
+class Node:
+
+    def __init__(self, data):
+
+        self.left = None
+        self.right = None
+        self.data = data
+ 
+# Insert method to create nodes
+    def insert(self, data):
+
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+# findval method to compare the value with nodes
+    def findval(self, lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                return False
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                return False
+            return self.right.findval(lkpval)
+        else:
+            return self.data
---
+++ main.py
@@ -0,0 +1,8 @@
+import BST
+
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+print(root.findval(7))
+print(root.findval(14))
